ec189fc9fdd482771c2908a251e179c2ae497ab4
NazaroBand <nazaroband1@gmail.com>
2026-09-10 20:16:15 +0200

    Fix typo in button text from 'Videe' to 'Videa'prepravak teksta

--- nazaro-band/src/components/Hero.tsx
+++ nazaro-band/src/components/Hero.tsx
@@ -152,7 +152,7 @@ export default function Hero() {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, delay: 0.8 }}
           >
-            Pogledaj Videe
+            Pogledaj Videa
           </motion.button>
         </div>
       </motion.div>
